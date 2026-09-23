26c7de7dc32a59cb5ae68edf00713f04bf86bc9f
pugalselvan04@gmail.com <pugalselvan04@gmail.com>
2026-09-23 13:39:34 +0530

    app.js changes in message

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const name = "learn pugal";
  
 app.get("/", (req, res) => {
-res.send("Welcome to ShopEasy pugal software Codetest ");
+    const welcomeMessage = `Welcome to ShopEasy ${name} software `;
+    res.send(welcomeMessage);
 });
  
 const server = app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
 });
-module.exports = { app, server };
+module.exports = { app, server,name };
--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,11 @@
 const request = require("supertest");
-const {app, server } = require("../app")
+const {app, server,name } = require("../app")
 
 describe("GET /", () => {
   it("should return a welcome message", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Welcome to ShopEasy pugal software ");
+    expect(response.text).toBe(`Welcome to ShopEasy ${name} software `);
   })
 })
 
